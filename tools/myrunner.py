ee9ed9d9ad852bab5e07b88b315f6f153a4092d0
bewagner <bewagner@davidson.edu@alpha.ada.davidson.edu>
2024-06-05 13:54:18 -0400

    Removed multi gpu functionality

--- tools/myrunner.py
+++ tools/myrunner.py
@@ -14,8 +14,8 @@ from extensions.chamfer_dist import ChamferDistanceL1, ChamferDistanceL2
 def run_net(args, config, train_writer=None, val_writer=None):
     logger = get_logger(args.log_name)
     # build dataset
-    (train_sampler, train_dataloader) = builder.dataset_builder(args, config.dataset.train)
-    (_, test_dataloader) = builder.dataset_builder(args, config.dataset.val)
+    train_dataloader = builder.dataset_builder(args, config.dataset.train)
+    test_dataloader = builder.dataset_builder(args, config.dataset.val)
     # build model
     base_model = builder.model_builder(config.model)
     if args.use_gpu:
@@ -50,17 +50,8 @@ def run_net(args, config, train_writer=None, val_writer=None):
             print_log(name, logger=logger)
     print_log('=' * 25, logger = logger)
 
-    # DDP
-    if args.distributed:
-        # Sync BN
-        if args.sync_bn:
-            base_model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(base_model)
-            print_log('Using Synchronized BatchNorm ...', logger = logger)
-        base_model = nn.parallel.DistributedDataParallel(base_model, device_ids=[args.local_rank % torch.cuda.device_count()], find_unused_parameters=True)
-        print_log('Using Distributed Data parallel ...' , logger = logger)
-    else:
-        print_log('Using Data parallel ...' , logger = logger)
-        base_model = nn.DataParallel(base_model).cuda()
+    print_log('Using Data parallel ...' , logger = logger)
+    base_model = nn.DataParallel(base_model).cuda()
     # optimizer & scheduler
     optimizer = builder.build_optimizer(base_model, config)
     
@@ -77,14 +68,12 @@ def run_net(args, config, train_writer=None, val_writer=None):
     # training
     base_model.zero_grad()
     for epoch in range(start_epoch, config.max_epoch + 1):
-        if args.distributed:
-            train_sampler.set_epoch(epoch)
-        base_model.train()
 
         epoch_start_time = time.time()
         batch_start_time = time.time()
         batch_time = AverageMeter()
         data_time = AverageMeter()
+
         losses = AverageMeter(['SparseLoss', 'DenseLoss'])
 
         num_iter = 0
@@ -92,7 +81,9 @@ def run_net(args, config, train_writer=None, val_writer=None):
         base_model.train()  # set model to training mode
         n_batches = len(train_dataloader)
         testing_flag = False
+
         for idx, (feats, labels) in enumerate(train_dataloader):
+            
             testing_flag = True
             data_time.update(time.time() - batch_start_time)
 
@@ -122,18 +113,10 @@ def run_net(args, config, train_writer=None, val_writer=None):
                 optimizer.step()
                 base_model.zero_grad()
 
-            if args.distributed:
-                sparse_loss = dist_utils.reduce_tensor(sparse_loss, args)
-                dense_loss = dist_utils.reduce_tensor(dense_loss, args)
-                losses.update([sparse_loss.item(), dense_loss.item()])
-            else:
-                losses.update([sparse_loss.item(), dense_loss.item()])
+            losses.update([sparse_loss.item(), dense_loss.item()])
 
             assert np.sum(np.isnan(losses.val())) == 0, f'NaN found in losses.val() at epoch {epoch}, batch {idx}'
             assert np.sum(np.isinf(losses.val())) == 0, f'Inf found in losses.val() at epoch {epoch}, batch {idx}'
-
-            if args.distributed:
-                torch.cuda.synchronize()
 
             n_itr = epoch * n_batches + idx
             if train_writer is not None:
@@ -209,19 +192,10 @@ def validate(base_model, test_dataloader, epoch, ChamferDisL1, ChamferDisL2, val
             dense_loss_l1 =  ChamferDisL1(dense_points, gt_pc)
             dense_loss_l2 =  ChamferDisL2(dense_points, gt_pc)
 
-            if args.distributed:
-                sparse_loss_l1 = dist_utils.reduce_tensor(sparse_loss_l1, args)
-                sparse_loss_l2 = dist_utils.reduce_tensor(sparse_loss_l2, args)
-                dense_loss_l1 = dist_utils.reduce_tensor(dense_loss_l1, args)
-                dense_loss_l2 = dist_utils.reduce_tensor(dense_loss_l2, args)
-
             test_losses.update([sparse_loss_l1.item(), sparse_loss_l2.item(), dense_loss_l1.item(), dense_loss_l2.item()])
 
             _metrics = Metrics.get(dense_points, gt_pc)
-            if args.distributed:
-                _metrics = [dist_utils.reduce_tensor(_metric, args).item() for _metric in _metrics]
-            else:
-                _metrics = [_metric.item() for _metric in _metrics]
+            _metrics = [_metric.item() for _metric in _metrics]
 
 
             if val_writer is not None and idx % 200 == 0:
@@ -246,9 +220,6 @@ def validate(base_model, test_dataloader, epoch, ChamferDisL1, ChamferDisL2, val
 
 
         print_log('[Validation] EPOCH: %d  Metrics = %s' % (epoch, ['%.4f' % m for m in test_metrics.avg()]), logger=logger)
-
-        if args.distributed:
-            torch.cuda.synchronize()
      
     # Print testing results
     print_log('============================ TEST RESULTS ============================',logger=logger)
@@ -278,17 +249,13 @@ crop_ratio = {
 def test_net(args, config):
     logger = get_logger(args.log_name)
     print_log('Tester start ... ', logger = logger)
-    _, test_dataloader = builder.dataset_builder(args, config.dataset.test)
+    test_dataloader = builder.dataset_builder(args, config.dataset.test)
  
     base_model = builder.model_builder(config.model)
     # load checkpoints
     builder.load_model(base_model, args.ckpts, logger = logger)
     if args.use_gpu:
         base_model.to(args.local_rank)
-
-    #  DDP    
-    if args.distributed:
-        raise NotImplementedError()
 
     # Criterion
     ChamferDisL1 = ChamferDistanceL1()
@@ -302,7 +269,6 @@ def test(base_model, test_dataloader, ChamferDisL1, ChamferDisL2, args, config, 
 
     test_losses = AverageMeter(['SparseLossL1', 'SparseLossL2', 'DenseLossL1', 'DenseLossL2'])
     test_metrics = AverageMeter(Metrics.names())
-    category_metrics = dict()
     n_samples = len(test_dataloader) # bs is 1
 
     with torch.no_grad():
@@ -321,6 +287,7 @@ def test(base_model, test_dataloader, ChamferDisL1, ChamferDisL2, args, config, 
 
             test_losses.update([sparse_loss_l1, sparse_loss_l2, dense_loss_l1, dense_loss_l2])
             _metrics = Metrics.get(dense_points, gt_pc)
+            _metrics = [_metric.item() for _metric in _metrics]
             test_metrics.update(_metrics)
 
             if (idx+1) % 200 == 0:
